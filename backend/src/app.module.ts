219f886808d1f3dc2e4d48c5bed0537552f2de04
donam1607 <dodinhnam160703@gmail.com>
2025-12-11 15:08:33 +0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { StudentsModule } from './students/students.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { ScoresModule } from './scores/scores.module';
-
 import { Student } from './students/student.entity';
 import { Subject } from './subjects/subject.entity';
 import { Score } from './scores/score.entity';
@@ -13,21 +11,27 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    // Load .env
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    // TypeORM dynamic config
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        url: config.get('DATABASE_URL'), // LẤY URL TỪ .env
-        ssl: { rejectUnauthorized: false }, // BẮT BUỘC CHO RENDER
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT', '5432'), 10),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+
         entities: [Student, Subject, Score],
         synchronize: false,
-        autoLoadEntities: true,
+
+        ssl: {
+          rejectUnauthorized: false, // ⭐ BẮT BUỘC CHO RENDER
+        },
       }),
     }),
 
@@ -35,7 +39,6 @@ import { AppController } from './app.controller';
     SubjectsModule,
     ScoresModule,
   ],
-
   controllers: [AppController],
 })
 export class AppModule {}
